1a14367603417ff8278cedb7181af9b3873d86c6
Marwan Musa <marwanlmusa@gmail.com>
2023-01-29 12:34:35 +0700

    and Task description

--- 30 Days Code Challenge/day28_regex_pattern.py
+++ 30 Days Code Challenge/day28_regex_pattern.py
@@ -6,7 +6,12 @@ import random
 import re
 import sys
 
-
+"""
+Task:
+    Consider a database table, Emails, which has the attributes First Name and Email ID.
+    Given N rows of data simulating the Emails table, print an alphabetically-ordered list of people
+    whose email address ends in @gmail.com.
+"""
 
 if __name__ == '__main__':
     N = int(input().strip())
