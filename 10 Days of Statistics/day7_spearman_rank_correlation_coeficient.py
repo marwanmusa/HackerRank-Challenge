2a4bb9d0329216500bf3fc11fa635700d70d93fa
Marwan Musa <marwanlmusa@gmail.com>
2023-03-28 09:43:41 +0700

    update Day 7: Spearman's Rank Correlation Coefficient solution

--- 10 Days of Statistics/day7_spearman_rank_correlation_coeficient.py
+++ 10 Days of Statistics/day7_spearman_rank_correlation_coeficient.py
@@ -14,7 +14,7 @@ def spearman_coef(X, Y, n):
     rank_X = [values+1 for values in {x:sorted_X.index(x) for x in X}.values()]
     rank_Y = [values+1 for values in {y:sorted_Y.index(y) for y in Y}.values()]
 
-    d = [x - y for y in rank_Y for x in rank_X if rank_Y.index(y) == rank_X.index(x)]
+    d = [x - y for (x,y) in zip(rank_X, rank_Y)]
 
     sum_d_square = sum([x**2 for x in d])
     res = 1 - ((6*sum_d_square)/(n*((n**2)-1)))
